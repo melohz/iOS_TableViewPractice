2afd070d4674ba4cf4959da051094543cdd2dfc4
Tom <melooh42@gmail.com>
2019-12-10 21:43:55 +0900

    Initial Commit

---
+++ TableViewPracticeTests/TableViewPracticeTests.swift
@@ -0,0 +1,34 @@
+//
+//  TableViewPracticeTests.swift
+//  TableViewPracticeTests
+//
+//  Created by 板垣智也 on 2019/12/10.
+//  Copyright © 2019 板垣智也. All rights reserved.
+//
+
+import XCTest
+@testable import TableViewPractice
+
+class TableViewPracticeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
