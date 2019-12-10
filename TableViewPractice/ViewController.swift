798d11ae28aa3af832fae9b8a95f02c5da4238c7
Tom <melooh42@gmail.com>
2019-12-10 21:57:45 +0900

    Add Simple TableView

--- TableViewPractice/ViewController.swift
+++ TableViewPractice/ViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let todo = ["一日やりきる", "諦めない心を持つ", "めげない"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    // セルの個数を指定する
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todo.count
+    }
+    
+    // セルに値を設定する
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルの取得
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        // セルにｓ表示する値を設定する
+        cell.textLabel!.text = todo[indexPath.row]
+        return cell
+    }
 }
 
